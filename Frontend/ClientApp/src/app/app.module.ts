d22630388aa45ebec74439e31be17b3dc33bd4eb
DavideCanton <davide.canton5@gmail.com>
2020-04-05 16:39:43 +0200

    sync with local storage

--- Frontend/ClientApp/src/app/app.module.ts
+++ Frontend/ClientApp/src/app/app.module.ts
@@ -4,12 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { EffectsModule } from '@ngrx/effects';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, ActionReducer, MetaReducer } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppComponent } from 'src/app/app.component';
 import { ArticlesEffects } from 'src/app/articles.effects';
 import { reducers } from 'src/app/reducers';
 import { environment } from 'src/environments/environment';
+import { localStorageSync } from 'ngrx-store-localstorage';
+
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any>
+{
+    return localStorageSync({
+        keys: ['articles'],
+        rehydrate: true
+    })(reducer);
+}
+const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
 
 @NgModule({
     declarations: [
@@ -22,7 +32,7 @@ import { environment } from 'src/environments/environment';
         RouterModule.forRoot([]),
         EffectsModule.forRoot([ArticlesEffects]),
         StoreModule.forRoot(reducers, {
-            metaReducers: [],
+            metaReducers,
             runtimeChecks: {
                 strictStateImmutability: true,
                 strictActionImmutability: true,
